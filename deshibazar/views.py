ffe32a2c6c83b1de1435bbcefaa244a5be35aca4
Akram Hossain <35065634+Coder-fox-bd@users.noreply.github.com>
2020-05-24 18:05:25 +0600

    ecommerce commit 3

--- deshibazar/views.py
+++ deshibazar/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-   return render(request, 'deshibazar/home.html', {}) 
+   return render(request, 'deshibazar/index.html', {}) 
